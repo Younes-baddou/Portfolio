0169fadd5ed0a6a610672f0c23e098ea01e047f8
Younes BADDOU <younes.baddou@supinfo.com>
2024-02-19 16:16:00 +0100

    added introduction and section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       I am currently a third-year student at SUPINFO Caen, focusing on honing my engineering skills. With a growing passion for web development since entering the field of computer science, I specialize in several areas. These include PHP & Laravel for robust back-end development, HTML/CSS for crafting intuitive and responsive user interfaces, and React for transforming ideas into dynamic applications with smooth user interactions. Additionally, I have expanded my skill set to include ASP.NET for building scalable web applications, Node.js for server-side programming, Docker for containerization and deployment, Terraform for infrastructure as code, and Microsoft Azure for cloud services. Actively seeking an apprenticeship, I aim to apply my diverse skills while continuing to learn and grow in web development. I am enthusiastic about challenging projects and learning opportunities that will further my development as a web developer, emphasizing collaboration, innovation, and perseverance. I firmly believe in the importance of knowledge sharing and teamwork for producing quality solutions.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
