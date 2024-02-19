cad491bc2e6f794871913471464d422df216394e
Younes BADDOU <younes.baddou@supinfo.com>
2024-02-19 19:30:17 +0100

    modified interface and post the tech and experiences

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,11 +20,11 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import terraform from "./tech/terraform.png";
+import microsoftazure from "./tech/microsoftazure.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import exakisnelite from "./company/exakisnelite.png";
+import rc2k from "./company/rc2k.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,11 +52,11 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
+  exakisnelite,
+  rc2k,
+  terraform,
+  microsoftazure,
 };
