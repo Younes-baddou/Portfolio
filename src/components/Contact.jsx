b94e0e534d29bcc24d52068ab0f4f22f023dbfe4
Younes BADDOU <younes.baddou@supinfo.com>
2024-02-20 16:52:56 +0100

    added earth and stars in the UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,11 +30,12 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
+// 
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_0q5ua6h',
+        'template_in7p1s8',
         {
           from_name: form.name,
           to_name: "Younes baddou",
@@ -42,7 +43,7 @@ const Contact = () => {
           to_email: "younesprobaddou@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'oxUlLsAA2cmsrYbD-'
       )
       .then(
         () => {
